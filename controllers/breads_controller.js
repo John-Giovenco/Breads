05154e62f84f0ecb0c95f94db7e40a9cf7fc6a4b
John-Giovenco <JohnGiovenco93@gmail.com>
2023-04-19 18:51:22 -0700

    adding bonus

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,14 +2,17 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// INDEX
-breads.get('/', (req, res) => {
-  res.render('Index',
-    {
-      breads: Bread
-    }
-  )
-// res.send(Bread)
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
 })
 
+
+// res.send(Bread)
 module.exports = breads
